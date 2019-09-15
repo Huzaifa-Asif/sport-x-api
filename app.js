65168db6dfca1dd80b960a638c8508ab70805eca
huzaifa85801 <huzaifa8580@gmail.com>
2019-09-15 18:15:49 +0500

    tournament backend added

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var routeShared = require('./app_server/routes/route.shared.js');
 var routeTeam = require('./app_server/routes/route.team.js');
 var routeTournament = require('./app_server/routes/route.tournament.js');
 var routeBookingSetting = require('./app_server/routes/route.bookingSetting.js');
+var routeFixture = require('./app_server/routes/route.fixture.js');
 
 
 var cors = require('cors')
@@ -72,6 +73,7 @@ app.use('/shared', routeShared);
 app.use('/team', routeTeam);
 app.use('/tournament', routeTournament);
 app.use('/bookingsetting',routeBookingSetting);
+app.use('/fixture',routeFixture);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
